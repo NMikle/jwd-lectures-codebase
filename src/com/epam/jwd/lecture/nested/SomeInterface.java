af17dd531b966381996b490fb42347164592b7f3
nmikle <nekrasovmikle@gmail.com>
2021-01-25 19:08:25 +0300

    static class usages in default and static methods

--- src/com/epam/jwd/lecture/nested/SomeInterface.java
+++ src/com/epam/jwd/lecture/nested/SomeInterface.java
@@ -4,6 +4,16 @@ public interface SomeInterface {
 
     void doSomething(OuterClass o);
 
+    default void someDefaultMethod() {
+        SubClass s = new SubClass();
+        final InnerEnum blue = InnerEnum.BLUE;
+    }
+
+    static void someStaticMethod() {
+        SubClass s = new SubClass();
+        final InnerEnum blue = InnerEnum.BLUE;
+    }
+
     class SubClass {
 
     }
