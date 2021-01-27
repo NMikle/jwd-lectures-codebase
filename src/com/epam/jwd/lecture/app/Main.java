4b66d994592399131df7cb5318936a073266b9e7
nmikle <nekrasovmikle@gmail.com>
2021-01-27 18:46:32 +0300

    few common classes and methods

--- src/com/epam/jwd/lecture/app/Main.java
+++ src/com/epam/jwd/lecture/app/Main.java
@@ -1,28 +1,52 @@
 package com.epam.jwd.lecture.app;
 
-import com.epam.jwd.lecture.nested.E;
-import com.epam.jwd.lecture.nested.OuterClass;
-import com.epam.jwd.lecture.nested.OuterClass.NestedClass;
-import com.epam.jwd.lecture.nested.OuterClass.NestedClass.AnotherNestedClass;
-import com.epam.jwd.lecture.nested.SomeInterface;
-
-import static com.epam.jwd.lecture.nested.OuterClass.NestedClass.nestedStaticMethod;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
     public static void main(String[] args) {
-        final NestedClass objectOfNestedType1 = new AnotherNestedClass();
-        final NestedClass objectOfNestedType2 = new E();
-        final NestedClass objectOfNestedType3 = new NestedClass() {
+//        final BigDecimal twentyFive = BigDecimal.valueOf(25);
+//        final BigDecimal result = twentyFive.divide(BigDecimal.valueOf(24), 2, RoundingMode.DOWN);
+//        System.out.println(result);
 
-            @Override
-            public void doSomething(OuterClass o) {
-                new E() {
+//        int a = new Integer(4);
+//        Integer b = 5;
+//        int c = Integer.parseInt("6");
+//        Integer d = Integer.valueOf("7");
+//        int e = new Integer("8");
+//        final Boolean trueOrFalse = new Boolean("tRuE");
+//
+//        if (new Integer("8") == new Integer("8")) {
+//            System.out.println("same");
+//        } else {
+//            System.out.println("different");
+//        }
+//
+//        if (new Integer("8") <= new Integer("8")) {
+//            System.out.println("fewer or equal");
+//        } else {
+//            System.out.println("greater");
+//        }
 
-                };
-            }
-        };
-        final int globalAnswer = nestedStaticMethod();
-        final SomeInterface.InnerEnum colorType = SomeInterface.InnerEnum.BLUE;
+//        final LocalDateTime now = LocalDateTime.now();
+//
+//        final LocalDateTime jwdEndsDate = now.plusMonths(2);
+//
+//        System.out.println(jwdEndsDate);
+
+        String a = "a";
+        String b = "a";
+        String c = "a";
+        String d = "a";
+
+        a = a + b;
+        c = a + c;
+        d = c + d;
+
+        final String result = new StringBuilder(a).append(b).append(c).append(d).toString();
     }
 }
