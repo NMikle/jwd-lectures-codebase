e000e67df4641832ec30594783fc6d21bd7a7474
nmikle <nekrasovmikle@gmail.com>
2021-02-08 13:31:04 +0300

    thread and runnable examples

--- src/com/epam/jwd/lecture/app/Main.java
+++ src/com/epam/jwd/lecture/app/Main.java
@@ -1,59 +1,29 @@
 package com.epam.jwd.lecture.app;
 
-import com.epam.jwd.lecture.model.BookingRole;
-import com.epam.jwd.lecture.model.BookingUser;
-import com.epam.jwd.lecture.pool.ConnectionPool;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import com.epam.jwd.lecture.concurent.RunnableSuccessor;
+import com.epam.jwd.lecture.concurent.ThreadSuccessor;
 
 public class Main {
 
-    private static final String SELECT_SQL = "select u.id as id, u.u_login as login, u.u_pass as password, u.u_name as name, r.role_name as role from b_user u join u_role r on r.id = u.role_id";
-
     public static void main(String[] args) {
-//        registerDrivers();
-        try (final Connection conn = ConnectionPool.INSTANCE.retrieveConnection();
-             final Statement statement = conn.createStatement();
-             final ResultSet resultSet = statement.executeQuery(SELECT_SQL)) {
-            while (resultSet.next()) {
-                final BookingUser user = new BookingUser(resultSet.getInt("id"),
-                        resultSet.getString("login"),
-                        resultSet.getString("password"),
-                        resultSet.getString("name"),
-                        BookingRole.of(resultSet.getString("role")));
-                System.out.println("user read successfully");
-                System.out.println(user);
-            }
-        } catch (SQLException e) {
-            System.out.println("user read unsuccessfully");
-            e.printStackTrace();
-        }
-//        deregisterDrivers();
+        final ThreadSuccessor firstThread = new ThreadSuccessor();
+        final Thread secondThread = new Thread(new RunnableSuccessor());
+
+        System.out.println("Hello from Main thread");
+
+        System.out.println("first thread state: " + firstThread.getState());
+        System.out.println("second thread state: " + secondThread.getState());
+
+        firstThread.start();
+
+        System.out.println("first thread state: " + firstThread.getState());
+        System.out.println("second thread state: " + secondThread.getState());
+
+        secondThread.start();
+
+        System.out.println("first thread state: " + firstThread.getState());
+        System.out.println("second thread state: " + secondThread.getState());
+
+        System.out.println("Hello again from Main thread");
     }
-
-//    private static void registerDrivers() {
-//        System.out.println("registering another drivers");
-//        try {
-//            DriverManager.registerDriver(DriverManager.getDriver("jdbc:mysql://localhost:1234/cafe"));
-//            System.out.println("registration successful");
-//        } catch (SQLException e) {
-//            System.out.println("deregistering successful");
-//            e.printStackTrace();
-//        }
-//    }
-
-//    private static void deregisterDrivers() {
-//        Enumeration<java.sql.Driver> drivers = DriverManager.getDrivers();
-//        while (drivers.hasMoreElements()) {
-//            try {
-//                DriverManager.deregisterDriver(drivers.nextElement());
-//            } catch (SQLException e) {
-//                System.out.println("deregistration unsuccessful");
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 }
