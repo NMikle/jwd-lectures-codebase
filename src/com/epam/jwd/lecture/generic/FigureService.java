a8330e4838b563c937a6f58f7a2440925c9bb516
nmikle <nekrasovmikle@gmail.com>
2021-02-22 19:56:11 +0300

    javadoc example

--- src/com/epam/jwd/lecture/generic/FigureService.java
+++ src/com/epam/jwd/lecture/generic/FigureService.java
@@ -2,8 +2,18 @@ package com.epam.jwd.lecture.generic;
 
 import com.epam.jwd.lecture.model.Figure;
 
+/**
+ * Service that stores figures and acts like an array
+ *
+ * @param <T> defines figure this service works with
+ */
 public interface FigureService<T extends Figure> extends Iterable<T> {
 
+    /**
+     * Method is used to retrieve all saved figures
+     *
+     * @return all found figures
+     */
     T[] findAll();
 
     void save(T figure);
